fd6fc8297d92a6ac3ad2f9c858a6657fe3c30342
AntoineDehan <antoine.dehandartial@gmail.com>
2026-06-07 10:12:36 +0200

    feat(reportService): report are now generable

--- backend/src/services/report.service.ts
+++ backend/src/services/report.service.ts
@@ -1,6 +1,8 @@
 import { prisma } from "../../prisma/lib/prisma";
 import type { Prisma } from "../../generated/prisma/client";
 import type { CreateReportInput } from "../schemas/report.schema";
+import { fetchLeetifyProfile } from "../lib/leetify";
+import { mapProfileToReport } from "../mappers/report_mapper";
 
 export function findAllReports() {
   return prisma.report.findMany();
@@ -14,6 +16,15 @@ export function createReport(data: CreateReportInput) {
   return prisma.report.create({
     data: data as Prisma.ReportUncheckedCreateInput,
   });
+}
+
+export async function generateReport(steam64Id: string, goalId: string) {
+  const leetifyProfile = await fetchLeetifyProfile(steam64Id);
+  if (leetifyProfile.privacy_mode === "private")
+    throw new Error("Leetify profile private");
+  const mappedStats = mapProfileToReport(leetifyProfile, goalId);
+
+  return await createReport(mappedStats);
 }
 
 export function updateReport(
