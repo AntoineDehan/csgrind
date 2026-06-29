9af8b974cfd29269c0ac4277e68712cbbb2005d5
AntoineDehan <antoine.dehandartial@gmail.com>
2026-06-29 18:31:30 +0200

    feat(main): added tanstack form and plugged in the QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { AuthProvider } from './auth/AuthProvider'
-import { router } from './router'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthProvider";
+import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
