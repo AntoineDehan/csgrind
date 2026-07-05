ab070f99683f80f925face1104674119aa1e7d01
AntoineDehan <antoine.dehandartial@gmail.com>
2026-07-05 10:36:05 +0200

    feat(homepage): added shacn and starter to build home layout

--- frontend/src/components/ui/Navbar/NavBar.tsx
+++ frontend/src/components/ui/Navbar/NavBar.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import type { ReactNode } from "react";
 import Logo from "../Logo/Logo";
+import Button from "../Button/Button";
 import styles from "./navbar.module.css";
 
 type NavItem = {
@@ -15,6 +16,8 @@ type NavbarProps = {
 };
 
 export default function Navbar({ logo = true, aside = [] }: NavbarProps) {
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.navbar}>
       {logo && (
@@ -24,15 +27,21 @@ export default function Navbar({ logo = true, aside = [] }: NavbarProps) {
       )}
 
       <div className={styles.aside}>
-        {aside.map((item) => (
-          <Link
-            key={item.to}
-            to={item.to}
-            className={item.cta ? styles.linkCta : styles.link}
-          >
-            {item.label}
-          </Link>
-        ))}
+        {aside.map((item) =>
+          item.cta ? (
+            <Button
+              key={item.to}
+              variant="cta"
+              onClick={() => navigate(item.to)}
+            >
+              {item.label}
+            </Button>
+          ) : (
+            <Link key={item.to} to={item.to} className={styles.link}>
+              {item.label}
+            </Link>
+          ),
+        )}
       </div>
     </nav>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,67 @@
+import Button from "../components/ui/Button/Button";
 import Title from "../components/ui/Title/Title";
+import Text from "../components/ui/Text/Text";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 
 export default function Home() {
-  return <Title>Home</Title>;
+  return (
+    <div className="flex align-item justify-center flex-col items-center pl-50 pr-50">
+      <div className="top-content flex justify-between">
+        <div>
+          <Title>See your progress, fix your mistakes</Title>
+          <div>
+            <Text>Description super génial ici</Text>
+          </div>
+        </div>
+        <div>Screen?</div>
+      </div>
+      <div className="main-content">
+        <div>Bilan img</div>
+      </div>
+      <div className="middle-content w-full h-96">
+        <Title>HOW TO START</Title>
+        <Text>3 easy steps before starting your journey to the top</Text>
+        <div className="cards-container flex gap-3 mt-6 justify-between">
+          <Card className="aspect-square size-[320px]">
+            <CardHeader>
+              <CardTitle>Register</CardTitle>
+              <CardDescription>First step is to register</CardDescription>
+            </CardHeader>
+            <CardContent>First step is to register</CardContent>
+          </Card>
+          <Card className="aspect-square size-[320px]">
+            <CardHeader>
+              <CardTitle>Set a Goal</CardTitle>
+              <CardDescription>
+                Set your elo goal and start to grind
+              </CardDescription>
+            </CardHeader>
+            <CardContent>First step is to register</CardContent>
+          </Card>
+          <Card className="aspect-square size-[320px]">
+            <CardHeader>
+              <CardTitle>Receive your Report</CardTitle>
+              <CardDescription>
+                Get a notification for your first report
+              </CardDescription>
+            </CardHeader>
+            <CardContent>First step is to register</CardContent>
+          </Card>
+        </div>
+      </div>
+      <div className="bottom-content">Pas sur ici</div>
+      <div>
+        <Button variant="cta">Register</Button>
+      </div>
+      <Title>Home</Title>
+    </div>
+  );
 }
